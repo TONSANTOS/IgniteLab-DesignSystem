8ba89eec0cea5c2852789224b26411aea3f5e0be
tonsantos <toncotias@icloud.com>
2022-10-16 12:58:54 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Envelope } from 'phosphor-react'
+
+import { TextInput } from './components/TextInput';
+import { Checkbox } from './components/Checkbox';
+
+import './styles/global.css';
+
+export function App() {
+
+  return (
+    <>
+      <TextInput.Root children={
+        <>
+          <TextInput.Icon>
+            <Envelope />
+          </TextInput.Icon>
+
+          <TextInput.Input placeholder="Hello World" />
+        </>
+      } />
+
+      <Checkbox />
+    </>
+  )
+}
